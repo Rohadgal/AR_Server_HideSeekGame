bee63a96cfebc65d5153677dc20efc30281a49fe
Rohadgal <gal.frias@gmail.com>
2023-09-16 17:08:28 -0600

    game set up with 3d models and win system

--- Assets/Scripts/findCreaturesGameManager.cs
+++ Assets/Scripts/findCreaturesGameManager.cs
@@ -1,3 +1,5 @@
+using Microsoft.Unity.VisualStudio.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class findCreaturesGameManager : MonoBehaviour
@@ -6,45 +8,67 @@ public class findCreaturesGameManager : MonoBehaviour
     [SerializeField]
     GameObject[] creatureModelsArray;
     [SerializeField]
-    UnityEngine.UI.Image[] imagesOnScreen;
+    GameObject[] imagesOnScreen;
     GameObject[] checkedCreaturesArray;
     #endregion
 
     #region Private
-    GameObject TargetsCreatureModel;
-    int index;
     LevelManager levelManager = LevelManager.s_instance;
+    int index;
     #endregion
 
     void Start()
     {
         index = 0;
+        checkedCreaturesArray = new GameObject[creatureModelsArray.Length];
+        for(int i = 0; i < checkedCreaturesArray.Length; i++) {
+            checkedCreaturesArray[i] = null;
+        }
+        for(int i = 0; i < imagesOnScreen.Length; i++) {
+            Debug.Log("Image Object name: " + imagesOnScreen[i].name);
+            Debug.Log("Creatures Model name: " + creatureModelsArray[i].name);
+        }
     }
 
     void Update()
     {
-       //checkIfGameFinished();
+       checkIfGameFinished();
     }
 
     void checkIfGameFinished() {
-        for (int i = 0; i < creatureModelsArray.Length; i++) {
-            if (creatureModelsArray[i] == null) {
-                return;
-            }
-            if (creatureModelsArray[i].activeSelf && checkedCreaturesArray[i] != creatureModelsArray[i]) {
-                imagesOnScreen[i].gameObject.SetActive(false);
-                checkedCreaturesArray[i] = creatureModelsArray[i];
-                Debug.Log("Thisssss");
+
+        if(index >= creatureModelsArray.Length) {
+            Debug.Log("Game Finished");
+            return;
+        }
+
+        for(int i = 0;i < creatureModelsArray.Length; i++) {
+            GameObject obj = creatureModelsArray[i];
+
+            if(obj.activeSelf && checkedCreaturesArray[i] == null) {
+                checkedCreaturesArray[i] = obj;
+                imagesOnScreen[i].SetActive(false);
                 index++;
             }
         }
+        //for (int i = 0; i < creatureModelsArray.Length; i++) {
+        //    if (creatureModelsArray[i] == null) {
+        //        return;
+        //    }
+        //    if (creatureModelsArray[i].activeSelf && checkedCreaturesArray[i] != creatureModelsArray[i]) {
+        //        imagesOnScreen[i].gameObject.SetActive(false);
+        //        checkedCreaturesArray[i] = creatureModelsArray[i];
+        //        Debug.Log("Thisssss");
+        //        index++;
+        //    }
+        //}
 
-        if (index >= creatureModelsArray.Length) {
-            levelManager.changeLevelState(LevelState.LevelFinished);
-        }
+        //if (index >= creatureModelsArray.Length) {
+        //    levelManager.changeLevelState(LevelState.LevelFinished);
+        //}
 
 
-        Debug.Log("Index: " + index);
+        //Debug.Log("Index: " + index);
 
     }
 }
